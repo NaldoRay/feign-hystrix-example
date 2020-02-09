881306bf5a5665e2ffee0a91ffed9a0cc7f943f2
Ray Naldo <narudor@gmail.com>
2020-02-09 23:11:40 +0700

    Enable hystrix dashboard

--- src/main/java/personal/narudore/example/feignhystrix/FeignHystrixExampleApplication.java
+++ src/main/java/personal/narudore/example/feignhystrix/FeignHystrixExampleApplication.java
@@ -2,10 +2,14 @@ package personal.narudore.example.feignhystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class FeignHystrixExampleApplication {
 
 	public static void main(String[] args) {
